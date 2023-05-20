61d5d18d7c5caec95ecbcecd10ef5a124b84a7f3
vivianmunguia <andy_munguia@hotmail.com>
2023-05-20 02:31:20 -0700

    The form was created and the signUP.css file was added

--- signUP.php
+++ signUP.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./signUP.css">
     <title>Sign UP</title>
 </head>
 <body>
     <div class="form">
-        <form action="" method="post"></form>
+        <form action="" method="post">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="lastName" placeholder="Last name">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="password" placeholder="Password">
+            <input type="text" name="confiPassword" placeholder="Confirm password">
+        </form>
     </div>
 </body>
 </html>
