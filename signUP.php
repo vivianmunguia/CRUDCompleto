e86cfc81ec70b762cd8b03a94736b2882a85f393
vivianmunguia <andy_munguia@hotmail.com>
2023-05-21 00:20:15 -0700

    Now the data can be inserted in the table

--- signUP.php
+++ signUP.php
@@ -8,6 +8,29 @@
     <title>Sign UP</title>
 </head>
 <body>
+    <?php
+        require('./conection.php');
+        if (isset($_POST['signUP_button'])) {
+            $name=$_POST['name'];
+            $lastName=$_POST['lastName'];
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+            $confPassword=$_POST['confiPassword'];
+            if (!empty($_POST['name']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+                if ($password == $confPassword) {
+                    $p = crud::conect()->prepare('INSERT INTO crudtable(name, lastName, email, pass) VALUES(:n, :l, :e, :p)');
+                    $p->bindValue(':n', $name);
+                    $p->bindValue(':l', $lastName);
+                    $p->bindValue(':e', $email);
+                    $p->bindValue(':p', $password);
+                    $p->execute();
+                    echo 'Successfully!';
+                } else {
+                    echo 'Password does not match!';
+                }         
+            }
+        }
+    ?>
     <div class="form">
         <div class="title">
             <p>Sign UP form</p>
@@ -15,9 +38,9 @@
         <form action="" method="post">
             <input type="text" name="name" placeholder="Name">
             <input type="text" name="lastName" placeholder="Last name">
-            <input type="text" name="email" placeholder="Email">
-            <input type="text" name="password" placeholder="Password">
-            <input type="text" name="confiPassword" placeholder="Confirm password">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="confiPassword" placeholder="Confirm password">
             <input type="submit" value="Sign UP" name="signUP_button">
         </form>
     </div>
